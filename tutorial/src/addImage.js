6114126ecafa377c6f8008b900f15a2e777d22dc
Sandhya Nair <sandhyanair411@gmail.com>
2023-05-05 18:12:29 +0400

    asset/source

--- tutorial/src/addImage.js
+++ tutorial/src/addImage.js
@@ -1,7 +1,9 @@
 import img from './image.jpeg'
+import altTxt from './altTxt.txt'
 function addImage(){
     const objImg = document.createElement('img');
     objImg.src=img;
+    objImg.alt=altTxt;
     objImg.width = '300';
     objImg.height = '300';
     const body = document.querySelector('body');
--- tutorial/webpack.config.js
+++ tutorial/webpack.config.js
@@ -5,15 +5,29 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname,"./dist"),
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "./dist/",
   },
   mode: "none",
-  module:{
-    rules:[
-        {
-            test: /\.(png|jpg|jpeg)$/,
-            type: 'asset/resource'
-        }
-    ]
-  }
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.txt/,
+        type: "asset/source",
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: "asset/inline",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024,
+          },
+        },
+      },
+    ],
+  },
 };
